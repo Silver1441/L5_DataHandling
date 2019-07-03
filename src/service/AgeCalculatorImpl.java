324a2ac7fbdc7549bf5583dbf4243d642298d329
Ivan <Kishkan91@gmail.com>
2019-07-03 21:54:12 +0400

    Problem 1.1 exception "invalid date" is added.

--- src/service/AgeCalculatorImpl.java
+++ src/service/AgeCalculatorImpl.java
@@ -21,35 +21,45 @@ public class AgeCalculatorImpl implements AgeCalculator {
         long minsDiff;
 
         LocalDateTime currentTime = LocalDateTime.now();
-        Period period = Period.between(birthDay.toLocalDate(), currentTime.toLocalDate());
-        yearsDiff = period.getYears();
-        monthsDiff = period.getMonths();
-        daysDiff = period.getDays();
 
-        if (currentTime.getHour()>=birthDay.getHour()) {
-            hoursDiff = currentTime.getHour() - birthDay.getHour();
-        } else {
-            hoursDiff = currentTime.getHour() - birthDay.getHour() + 24;
-            daysDiff -= daysDiff;
-        }
+        if (currentTime.isAfter(birthDay)) {
 
-        if (currentTime.getMinute()>=birthDay.getMinute()) {
-            minsDiff = currentTime.getMinute() - birthDay.getMinute();
-        } else {
-            minsDiff = currentTime.getMinute() - birthDay.getMinute() + 60;
-            if (hoursDiff!=0) {
-                hoursDiff -= hoursDiff;
+            Period period = Period.between(birthDay.toLocalDate(), currentTime.toLocalDate());
+            yearsDiff = period.getYears();
+            monthsDiff = period.getMonths();
+            daysDiff = period.getDays();
+
+            if (currentTime.getHour()>=birthDay.getHour()) {
+                hoursDiff = currentTime.getHour() - birthDay.getHour();
             } else {
-                hoursDiff = 23;
+                hoursDiff = currentTime.getHour() - birthDay.getHour() + 24;
                 daysDiff -= daysDiff;
             }
+
+            if (currentTime.getMinute()>=birthDay.getMinute()) {
+                minsDiff = currentTime.getMinute() - birthDay.getMinute();
+            } else {
+                minsDiff = currentTime.getMinute() - birthDay.getMinute() + 60;
+                if (hoursDiff!=0) {
+                    hoursDiff -= hoursDiff;
+                } else {
+                    hoursDiff = 23;
+                    daysDiff -= daysDiff;
+                }
+            }
+            System.out.println(" Current age:");
+            System.out.println("Years: " + yearsDiff);
+            System.out.println("Months: " + monthsDiff);
+            System.out.println("Days: " +  daysDiff);
+            System.out.println("Hours: " + hoursDiff);
+            System.out.println("Minutes: " + minsDiff);
+
+        } else {
+            System.out.println("exception: invalid birthday date");
         }
 
-        System.out.println(" Current age:");
-        System.out.println("Years: " + yearsDiff);
-        System.out.println("Months: " + monthsDiff);
-        System.out.println("Days: " +  daysDiff);
-        System.out.println("Hours: " + hoursDiff);
-        System.out.println("Minutes: " + minsDiff);
+
+
+
     }
 }
