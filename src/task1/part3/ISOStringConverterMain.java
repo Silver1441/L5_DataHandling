6eaecba89c6004c516e52a5ff9470fccd712108e
Ivan <Kishkan91@gmail.com>
2019-07-09 01:46:00 +0400

    problem 1.3 refactored again

--- src/task1/part3/ISOStringConverterMain.java
+++ src/task1/part3/ISOStringConverterMain.java
@@ -4,7 +4,6 @@ import task1.part3.service.ISODateFormatter;
 import static  task1.part3.service.ISOStringDemo.makeTest;
 
 public class ISOStringConverterMain {
-
     public static void main(String[] args) {
         makeTest(ISODateFormatter.formatDate("Wednesday, Feb 10, 2016 1:10:56 PM"));
     }
